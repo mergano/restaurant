aed8d116a7f58487964b520b8c4bb3dfd2132543
jukbot <vchukkrit@hotmail.com>
2016-03-25 16:46:56 +0700

    Fix enter key action event broken

--- src/core/LogInFrame.java
+++ src/core/LogInFrame.java
@@ -200,6 +200,7 @@ public class LogInFrame extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new LogInFrame().setVisible(true);
             }
